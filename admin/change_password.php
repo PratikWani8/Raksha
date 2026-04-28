b2f1eddc39f36c3bdc321f0661253811354da785
Pratik Wani <pwani8306@gmail.com>
2026-04-28 19:16:38 +0530

    update change password

--- admin/change_password.php
+++ admin/change_password.php
@@ -36,10 +36,9 @@ if (isset($_POST['change'])) {
     <title>Change Admin Password - Raksha</title>
     <link rel="stylesheet" href="../style.css?v=3">
 
-    <!-- META TAGS -->
-
-    <meta charset = "UTF-8" >
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- META TAGS -->
+<meta charset = "UTF-8" >
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     
 <meta name="title" content="Raksha - Women Safety & Emergency Protection System">
 <meta name="description" content="Raksha is a smart women safety platform for SOS alerts, emergency support, live location sharing, and nearby police assistance. Stay safe, stay empowered.">
